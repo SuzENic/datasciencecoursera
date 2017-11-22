11dcd182d3dad56a53466bd7090b0c154ab56055
Suzanne Nichols <senichol@Suzannes-MacBook-Air.local>
2017-11-22 16:35:12 -0500

    finished HW Week 2 R programming

--- corr.R
+++ corr.R
@@ -1,3 +1,23 @@
 corr <- function(directory, threshold = 0) {
-
+     id <- 1:332 
+     for(i in id) {
+     	   num <- complete(directory,i)
+	   if (num[2] > threshold) {
+	      name<- paste(directory, formatC(i, width=3, flag="0"), ".csv", sep = "")
+              file_data <-read.csv(file = name)
+              outp <- (!is.na(file_data[,1])) & (!is.na(file_data[,2])) & (!is.na(file_data[,3])) & (!is.na(file_data[,4]))
+	      out <- cor(file_data[outp,2], file_data[outp,3])
+              if(exists("corbs") == FALSE) {
+              	    corbs <- out
+	      } else {
+                    corbs <- c(corbs,out)
+              }
+ 
+           }
+	}            
+#     cor(new_list_S,new_list_N)
+      if(exists("corbs") == FALSE) {
+      	corbs <- vector(mode="numeric",length =0)
+	}		
+      corbs
 }
--- pollutantmean.R
+++ pollutantmean.R
@@ -1,3 +1,14 @@
 pollutantmean <- function(directory, pollutant, id = 1:332){
-
+	      for(i in id){
+	      	    name<- paste(directory, formatC(i, width=3, flag="0"), ".csv", sep = "")	      
+	      	    file_data <-read.csv(file = name)
+	      	    nots <- is.na(file_data[,pollutant])
+	      	    list <- file_data[!nots, pollutant]
+		    if(i == id[1]) {
+	      	    new_list <- list
+		    } else {
+		    new_list <- c(new_list,list)
+		    }
+	      }
+	      mean(new_list)
 }
